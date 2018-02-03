c2dedce6f44745763ff557481574e64969abaaf2
Sergio Benitez <sb@sergio.bz>
2018-02-02 21:34:34 -0800

    Phase 4. Improve UART library documentation.

---
+++ bootloader/src/kmain.rs
@@ -0,0 +1,29 @@
+#![feature(asm, lang_items)]
+
+extern crate xmodem;
+extern crate pi;
+
+pub mod lang_items;
+
+/// Start address of the binary to load and of the bootloader.
+const BINARY_START_ADDR: usize = 0x80000;
+const BOOTLOADER_START_ADDR: usize = 0x4000000;
+
+/// Pointer to where the loaded binary expects to be laoded.
+const BINARY_START: *mut u8 = BINARY_START_ADDR as *mut u8;
+
+/// Free space between the bootloader and the loaded binary's start address.
+const MAX_BINARY_SIZE: usize = BOOTLOADER_START_ADDR - BINARY_START_ADDR;
+
+/// Branches to the address `addr` unconditionally.
+fn jump_to(addr: *mut u8) -> ! {
+    unsafe {
+        asm!("br $0" : : "r"(addr as usize));
+        loop { asm!("nop" :::: "volatile")  }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn kmain() {
+    // FIXME: Implement the bootloader.
+}
