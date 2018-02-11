d6bac11157b324d45b53164e99ec515fd6b5a24f
Jeff McGlynn <jeff@jeffmcglynn.com>
2018-02-10 23:01:05 -0800

    Implement timer and test it in kmain.

--- os/pi/src/timer.rs
+++ os/pi/src/timer.rs
@@ -30,21 +30,27 @@ impl Timer {
     /// Reads the system timer's counter and returns the 64-bit counter value.
     /// The returned value is the number of elapsed microseconds.
     pub fn read(&self) -> u64 {
-        unimplemented!()
+        return (self.registers.CHI.read() as u64) << 32
+                | (self.registers.CLO.read() as u64);
     }
 }
 
 /// Returns the current time in microseconds.
 pub fn current_time() -> u64 {
-    unimplemented!()
+    Timer::new().read()
 }
 
 /// Spins until `us` microseconds have passed.
 pub fn spin_sleep_us(us: u64) {
-    unimplemented!()
+    let timer = Timer::new();
+    let target: u64 = timer.read() + us;
+
+    while timer.read() < target {
+        // Spin...
+    }
 }
 
 /// Spins until `ms` milliseconds have passed.
 pub fn spin_sleep_ms(ms: u64) {
-    unimplemented!()
+    return spin_sleep_us(ms * 1000);
 }
