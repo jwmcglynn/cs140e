2bb5014af95b080933faf9b9656e3918458eb117
Sergio Benitez <sb@sergio.bz>
2018-02-12 22:11:46 -0800

    Add the missing 'col' argument to 'panic_fmt'.

--- kernel/src/lang_items.rs
+++ kernel/src/lang_items.rs
@@ -1,7 +1,7 @@
 #[no_mangle]
 #[cfg(not(test))]
 #[lang = "panic_fmt"]
-pub extern fn panic_fmt(fmt: ::std::fmt::Arguments, file: &'static str, line: u32) -> ! {
+pub extern fn panic_fmt(fmt: ::std::fmt::Arguments, file: &'static str, line: u32, col: u32) -> ! {
     // FIXME: Print `fmt`, `file`, and `line` to the console.
 
     loop { unsafe { asm!("wfe") } }
