400b948b99fd606718f072c98bb84cfba5034df4
Sergio Benitez <sb@sergio.bz>
2018-02-14 04:05:54 -0800

    Assignment 2 phase 1{c,d,e}.

--- kernel/src/console.rs
+++ kernel/src/console.rs
@@ -67,9 +67,15 @@ pub static CONSOLE: Mutex<Console> = Mutex::new(Console::new());
 /// Internal function called by the `kprint[ln]!` macros.
 #[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
-    use std::fmt::Write;
-    let mut console = CONSOLE.lock();
-    console.write_fmt(args).unwrap();
+    #[cfg(not(test))]
+    {
+        use std::fmt::Write;
+        let mut console = CONSOLE.lock();
+        console.write_fmt(args).unwrap();
+    }
+
+    #[cfg(test)]
+    { print!("{}", args); }
 }
 
 /// Like `println!`, but for kernel-space.
