41ef36e424a61f61234320e0cb8870914ea8b4de
Sergio Benitez <sb@sergio.bz>
2018-03-05 01:06:50 -0800

    Only link SD driver on AArch64 builds. Trust in LTO.

--- kernel/build.rs
+++ kernel/build.rs
@@ -1,6 +1,10 @@
 pub fn main() {
-    println!("cargo:rustc-link-search=native=ext");
-    println!("cargo:rustc-link-lib=static=sd");
+    if ::std::env::var("TARGET").unwrap() == "aarch64-none-elf" {
+        println!("cargo:rustc-link-search=native=ext");
+        println!("cargo:rustc-link-lib=static=sd");
+        println!("cargo:rerun-if-changed=ext/libsd.a");
+    }
+
     println!("cargo:rerun-if-changed=ext/layout.ld");
     println!("cargo:rerun-if-changed=ext/init.S");
 }
